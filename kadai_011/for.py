5e1bb3f348c751603cfc2d1346161cab51aec9c4
mshtwtnb0219 <b77yellow@gmail.com>
2023-11-22 13:50:56 +0900

    訂正

--- kadai_011/for.py
+++ kadai_011/for.py
@@ -1,12 +1,9 @@
 list = ["水","金","地","火","木","土","天","海","冥"]
 
-for list in list:
-    print(list)
-    
 i = 0
 while i < len(list):
     print(list[i])
     i += 1
-
-    
-
+print("-----------------------------------") 
+for list in list:
+    print(list) 

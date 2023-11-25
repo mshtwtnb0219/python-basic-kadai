56bf4f06d71a68646cfc164d74783567741f5a61
mshtwtnb0219 <b77yellow@gmail.com>
2023-11-25 12:21:55 +0900

    課題17ｺﾐｯﾄ

---
+++ kadai_017/summary.py
@@ -0,0 +1,18 @@
+class Human:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+    def check_adult(self):
+        if self.age >= 20:
+            print(f"名前 {self.name} ：年齢{self.age}大人です")
+        else:
+            print(f"名前 {self.name} ：年齢{self.age}未成年です")
+
+
+#複数人分のリスト
+humans = [Human("太郎",10),Human("次郎",20),Human("三郎",15)]
+
+for human in humans:
+    human.check_adult()
+
